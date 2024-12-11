59c421e9087fbe3865dd1d07c9b87997a09de584
0x67 <evilisnj@gmail.com>
2024-12-11 10:35:03 +0800

    ios 18 fix.

--- DeepLinkDemoApp/ContentView.swift
+++ DeepLinkDemoApp/ContentView.swift
@@ -1,31 +1,45 @@
-// 
+//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      Form {
-        
-        Link("Setting Safari", destination: URL(string: "App-Prefs:Safari")!)
-       
-        Link("Extension Settings Safari", destination: URL(string: "App-Prefs:Safari&path=WEB_EXTENSIONS")!)
-        
-        Link("iCloud Account", destination: URL(string: "App-Prefs:APPLE_ACCOUNT")!)
-
-      }
-      .onAppear {
-        print("s:", UIApplication.openSettingsURLString)
-      }
+        Form {
+            
+            if #available(iOS 18.0, *) {
+                
+                // iOS 18+ has a standalone App section in the settings, workaround is using specific bundle id
+                
+                Link("Setting Safari", destination: URL(string: "App-Prefs:com.apple.mobilesafari")!)
+                
+                Link("Extension Settings Safari", destination: URL(string: "App-Prefs:com.apple.mobilesafari&path=WEB_EXTENSIONS")!)
+                
+                Link("iCloud Account", destination: URL(string: "App-Prefs:APPLE_ACCOUNT")!)
+                
+            } else {
+                
+                Link("Setting Safari", destination: URL(string: "App-Prefs:Safari")!)
+                
+                Link("Extension Settings Safari", destination: URL(string: "App-Prefs:Safari&path=WEB_EXTENSIONS")!)
+                
+                Link("iCloud Account", destination: URL(string: "App-Prefs:APPLE_ACCOUNT")!)
+                
+            }
+            
+        }
+        .onAppear {
+            print("s:", UIApplication.openSettingsURLString)
+        }
     }
     
-  private func open(urlString: String) {
-    let url = URL(string: urlString)!
-    guard UIApplication.shared.canOpenURL(url) else {
-      assertionFailure()
-      return
+    private func open(urlString: String) {
+        let url = URL(string: urlString)!
+        guard UIApplication.shared.canOpenURL(url) else {
+            assertionFailure()
+            return
+        }
+        UIApplication.shared.open(url)
     }
-    UIApplication.shared.open(url)
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
